043129f0884210300e0db92d7f86a36db1c35fcb
Juan Manuel Carpio Baez <jmcarpiob@gmail.com>
2020-01-29 14:57:05 +0100

    [FIX] Babel config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: ['@babel/typescript', '@babel/env'],
     plugins: [
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread',
+        '@babel/plugin-transform-typescript',
         [
             'module-resolver',
             {
